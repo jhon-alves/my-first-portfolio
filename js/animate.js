741bee19fc5f31412a4026f03402d20612fcf917
Jonatas Alves De Oliveira <jonadol@br.ibm.com>
2020-11-26 01:13:16 -0300

    Add animate scroll

---
+++ js/animate.js
@@ -0,0 +1,21 @@
+const target = document.querySelectorAll('[data-animate]');
+const animationClass = 'anime';
+
+function animeScroll() {
+  const windowTop = window.pageYOffset + ((window.innerHeight + 1) / 1.5);
+  target.forEach(function(element) {
+    if((windowTop) > element.offsetTop) {
+      element.classList.add(animationClass);
+    } else {
+      element.classList.remove(animationClass);
+    }
+  });
+}
+
+animeScroll();
+
+if(target.length){
+  window.addEventListener('scroll', function() {
+    animeScroll();
+  });
+}
